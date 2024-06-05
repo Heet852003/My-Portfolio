82776056ec9cc48d29b5249e3743e5a9630dd53a
Heet852003 <95292118+Heet852003@users.noreply.github.com>
2024-06-05 18:00:36 +0530

    Add files via upload

---
+++ portfoliowe.js
@@ -0,0 +1,36 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Responsive navigation
+const menuBtn = document.querySelector('.menu-btn');
+const navLinks = document.querySelector('.nav-links');
+
+menuBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+// Reveal sections on scroll
+const revealSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+entry.target.classList.add('fade-in');
+observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+root: null,
+threshold: 0.15,
+});
+revealSections.forEach(function (section) {
+sectionObserver.observe(section);
+});
