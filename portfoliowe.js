f47e4a3964ec41f6a35d9d9f9cee80448654e5c2
Heet Mehta <95292118+Heet852003@users.noreply.github.com>
2024-09-12 16:30:16 +0530

    Update portfoliowe.js

--- portfoliowe.js
+++ portfoliowe.js
@@ -100,15 +100,12 @@ window.addEventListener('scroll', () => {
   }
 });
 
-backToTopButton.addEventListener('click', () => {
-  const scrollToTop = () => {
-    const c = document.documentElement.scrollTop || document.body.scrollTop;
-    if (c > 0) {
-      window.requestAnimationFrame(scrollToTop);
-      window.scrollTo(0, c - c / 8);
-    }
-  };
-  scrollToTop();
+backToTopButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
 });
 
 // Typing effect for hero section
