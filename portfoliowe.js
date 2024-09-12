88a730e2027260d482689705c1308725c000b11f
Heet Mehta <95292118+Heet852003@users.noreply.github.com>
2024-09-12 16:13:56 +0530

    Update portfoliowe.js

--- portfoliowe.js
+++ portfoliowe.js
@@ -405,7 +405,23 @@ footer {
 .scroll-to-top:hover {
   background-color: #ffd700;
 }
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
 
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    const headerOffset = 80; // Adjust this value based on your header height
+    const elementPosition = targetElement.getBoundingClientRect().top;
+    const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth'
+    });
+  });
+});
 /* Responsive styles */
 @media (max-width: 768px) {
   .menu-btn {
